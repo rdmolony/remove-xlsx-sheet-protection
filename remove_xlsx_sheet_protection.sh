67f93590408c391ac6d086946a6bc9bd7c6d6e7b
Rowan Molony <rowan.molony@codema.ie>
2020-11-11 14:18:38 +0000

    Add Excel unlocker
    Unlocks all protected sheets in an Excel xlsx file

---
+++ remove_xlsx_sheet_protection.sh
@@ -0,0 +1,16 @@
+temp_dir=$(echo $1 | cut -f 1 -d '.')
+echo '\nUnzipping '"$1"' to '"$temp_dir"' ...\n'
+unzip $1 -d $temp_dir 
+
+echo '\nRemoving sheet protection ...\n'
+cd $temp_dir/xl/worksheets
+sed -i 's/<sheetProtection [^>]*//g' *.xml
+
+unlocked_file=$temp_dir'_unlocked.xlsx'
+echo '\nSaving results as '"$unlocked_file"' ...'
+cd ../..
+find . -type f | xargs zip ../$unlocked_file
+cd ..
+
+echo '\nRemoving '"$temp_dir"' ...\n'
+rm -fr $temp_dir
